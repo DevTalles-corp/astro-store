bca74e144a6eea182cfcfb248881a5b202ee7e5d
Fernando Herrera <fernando.herrera85@gmail.com>
2024-07-01 10:16:49 -0400

    chore: Add credentials-based authentication with Astro

--- auth.config.ts
+++ auth.config.ts
@@ -1,5 +1,8 @@
 // import GitHub from '@auth/core/providers/github';
+import { User, db, eq } from 'astro:db';
 import { defineConfig } from 'auth-astro';
+import Credentials from '@auth/core/providers/credentials';
+import bcrypt from 'bcryptjs';
 
 export default defineConfig({
   providers: [
@@ -8,5 +11,30 @@ export default defineConfig({
     //   clientId: import.meta.env.GITHUB_CLIENT_ID,
     //   clientSecret: import.meta.env.GITHUB_CLIENT_SECRET,
     // }),
+
+    Credentials({
+      credentials: {
+        email: { label: 'Correo', type: 'email' },
+        password: { label: 'Contraseña', type: 'password' },
+      },
+      authorize: async ({ email, password }) => {
+        const [user] = await db
+          .select()
+          .from(User)
+          .where(eq(User.email, `${email}`));
+
+        if (!user) {
+          throw new Error('User not found');
+        }
+
+        if (!bcrypt.compareSync(password as string, user.password)) {
+          throw new Error('Invalid password');
+        }
+
+        const { password: _, ...rest } = user;
+        console.log({ rest });
+        return rest;
+      },
+    }),
   ],
 });
