71454f8c8ac6a5d1421a28b5d628b17d8a6d1bfa
Fernando Herrera <fernando.herrera85@gmail.com>
2024-07-01 10:05:28 -0400

    chore: Add authentication integration with Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import netlify from "@astrojs/netlify";
-
 import db from "@astrojs/db";
+
+import auth from "auth-astro";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), db()],
+  integrations: [tailwind(), db(), auth()],
   output: "server",
   adapter: netlify(),
   experimental: {
