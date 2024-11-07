978a9e480b74bf55ef9da7945970e3abb41d3129
Fernando Herrera <fernando.herrera85@gmail.com>
2024-11-07 16:04:13 -0500

    chore: Update Astro configuration to reorder integrations and upgrade db dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import netlify from "@astrojs/netlify";
-import db from "@astrojs/db";
 import auth from "auth-astro";
 
 import react from "@astrojs/react";
 
+import db from "@astrojs/db";
+
 // https://astro.build/config
 export default defineConfig( {
-  integrations: [ tailwind(), db(), auth(), react() ],
+  integrations: [ tailwind(), auth(), react(), db() ],
   output: "server",
   adapter: netlify(),
 } );
